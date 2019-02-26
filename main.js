d9ca7318d32a125749c458ff976385822450bd67
Sanjay Kumar Singh01 <sanjay.singh01@irissoftware.com>
2019-02-26 18:27:20 +0530

    updated using jquery

--- main.js
+++ main.js
@@ -47,3 +47,58 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "https://www.cbc.ca/json/brexit-negotiations-foreign-secretary-jeremy-hunt-1.5000210", true);
 xmlhttp.send();
+
+
+
+
+//using jQuery
+
+// $(document).ready(function(){
+//   $.ajax({
+//     type: "GET",
+//     url: "https://www.cbc.ca/json/brexit-negotiations-foreign-secretary-jeremy-hunt-1.5000210", 
+//     success: function(data) {
+//     console.log(data);
+
+//     var getSlots=data.slots;
+
+//     var filterData=getSlots.filter((value) => {
+//         return value.elements.length !== 0;
+//     })
+
+//     var filterArticalData=filterData[0].elements[0];
+
+//     var getBodyContents=filterArticalData.body.split(' ');
+//     var replaceBodyContent;
+
+//     var getStoryImages=filterArticalData.storyimages;
+
+//       getStoryImages.forEach((value)=>{
+//         replaceBodyContent = getBodyContents.map((item) => {
+//           if(item == '<p>[IMAGE]</p>'){
+//             return '<div><img src=' + value.originalimageurl +'></div>';
+//           }else{
+//             return item;
+//           }
+//         })
+//     })
+
+//     var replaceBodyContentWithImage=replaceBodyContent.join(' ');
+
+//     $('#heading').html(filterArticalData.originalheadline);
+//     $('#subHeading').html(filterArticalData.deck);
+//     $('#body').html(replaceBodyContentWithImage);
+
+//     var getFirstStoryImage=getStoryImages[0].originalimageurl;
+//     var getFirstStoryCaption=getStoryImages[0].description;
+    
+//     $('#image').attr('src',getFirstStoryImage);
+//     $('#imgCaption').html(getFirstStoryCaption);
+
+//   },
+//     error:function(err){
+//       console.log(err);
+//     }
+//   });
+// });
+
